9acde6b751e1e01936788f824a286126ef6ab645
Joe Rehfuss <jrehfuss@c02g81atq05q.domain>
2022-11-09 12:57:33 -0800

    adding login/signup routes

--- controllers/frontEndRoutes.js
+++ controllers/frontEndRoutes.js
@@ -2,8 +2,20 @@ const express = require('express');
 const router = express.Router();
 const {User,Flavor} = require('../models');
 
+//TODO: pass in auth state
 router.get('/',(req,res)=>{
     res.render("home")
 })
+router.get('/login',(req,res)=>{
+    //TODO: redirect authenticated users away from this page
+    res.render("login")
+})
+
+//signup
+router.get('/signup',(req,res)=>{
+    //TODO: redirect authenticated users away from this page
+    res.render("signup")
+})
+//profile
 
 module.exports = router;
