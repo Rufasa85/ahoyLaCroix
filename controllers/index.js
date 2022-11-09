c0ce67f55941ae17b32e3f7e2ccd974927c20b6b
Joe Rehfuss <jrehfuss@c02g81atq05q.domain>
2022-11-09 12:39:16 -0800

    adding handlebars setup

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const userRoutes = require("./userRoutes");
 router.use("/api/users",userRoutes)
+const frontEndRoutes = require("./frontEndRoutes");
+router.use("/",frontEndRoutes)
 
 router.get("/session",(req,res)=>{
     res.json(req.session)
